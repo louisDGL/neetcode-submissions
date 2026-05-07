241dd874cfe2f3696bf884e2c4b9bd6d68d087b0
Darrigol Louis <louis.darrigol@polytechnique.edu>
2026-05-07 20:35:01 +0200

    Bulk sync: 18 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        idx = 1
+        n = len (nums)
+
+        while idx < n:
+            if nums[idx] == nums[idx - 1]:
+                return True
+            idx += 1
+        
+        return False
