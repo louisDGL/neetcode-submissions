9500ac76d6ca0b98e753ef736c7cce61e65ff0fd
Darrigol Louis <louis.darrigol@polytechnique.edu>
2026-06-19 02:31:17 +0200

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack([], nums, 0)
+        return self.res
+    
+    def backtrack(self, perms: List[int], nums: List[int], mask: int):
+        if len(perms) == len(nums):
+            self.res.append(perms[:])
+            return
+        for i in range(len(nums)):
+            if not (mask & (1 << i)):
+                perms.append(nums[i])
+                self.backtrack(perms, nums, mask | (1 << i))
+                perms.pop()
